50c12497662c0f91e56b0433b2ece2b30bebeb25
badpolymer <roans_policy.07@icloud.com>
2023-12-11 19:48:08 -0500

    add Update and Create For Customer API

--- backend/src/strategy/postgresql/api/CustomerApi.ts
+++ backend/src/strategy/postgresql/api/CustomerApi.ts
@@ -1,5 +1,5 @@
 // Written by Frederick
-// Version 1
+// Version 2
 // Last update: 2023-12-11
 import { DataSource } from "typeorm";
 import { Customer } from "../models/Customer";
@@ -12,15 +12,58 @@ export default class CustomerApi {
         this.#dataConnector = dataConnector;
     }
 
-    async getCustomerByID(id: number) {
-        if (isNaN(id) || id < 0 ) {
-            return Error(`The id is not valid.`)
+    async getCustomerByID(id: number): Promise<Customer> {
+        if (isNaN(id) || id < 0) {
+            throw new Error(`The ID is not valid. Code: CG000`)
         }
-        const result = await this.#dataConnector.get({ id: id })
-        if (result instanceof Error) {
-            return result
+        const result = await this.#dataConnector.get([{ id: id }])
+        if (result.length >= 1) {
+            return result[0]
+        } else {
+            throw new Error(`Customer with ID ${id} not found. Code: CG001`)
         }
-        return result[0]
+
+    }
+
+    async getCustomers(predicates: Object[]): Promise<Customer[]> {
+        const result = await this.#dataConnector.get(predicates);
+        return result;
+    }
+
+    async create(name: string, address: string, phoneNumber1: string,
+        phoneNumber2: string): Promise<Customer> {
+
+        var customer = new Customer();
+        customer.name = name;
+        customer.address = address;
+        customer.phoneNumber1 = phoneNumber1;
+        customer.phoneNumber2 = phoneNumber2
+
+        await this.#dataConnector.save(customer);
+
+        return customer;
+
+    }
+
+    async updateByID(id: number, name: string, address: string,
+        phoneNumber1: string, phoneNumber2: string): Promise<Customer> {
+
+        if (isNaN(id) || id < 0) {
+            throw new Error(`The ID is not valid. Code: CU002`)
+        }
+
+        const result = await this.#dataConnector.get([{ id: id }])
+
+        var customer = result[0];
+        customer.name = name;
+        customer.address = address;
+        customer.phoneNumber1 = phoneNumber1;
+        customer.phoneNumber2 = phoneNumber2;
+
+        await this.#dataConnector.save(customer);
+
+        return customer;
+
     }
 
 
@@ -36,20 +79,20 @@ export class CustomerDataConnector implements DataConnector<Customer>{
         this.#dataSource = dataSource;
     }
 
-    async get(predicate: Object): Promise<Customer[] | Error> {
+    async get(predicates: Object[]): Promise<Customer[]> {
         try {
-            return await this.#dataSource.manager.findBy(Customer, predicate)
+            return await this.#dataSource.manager.findBy(Customer, predicates)
         } catch (e) {
-            return Error(`Error occured when searching. Code: CS000`)
+            throw Error(`Error occured when searching. Code: CS000`)
         }
 
     }
 
-    async save(entity: Customer): Promise<void | Error> {
+    async save(entity: Customer): Promise<void> {
         try {
             await this.#dataSource.manager.save(entity);
         } catch (e) {
-            return Error(`Error occured when saving. Code: CS001`)
+            throw Error(`Error occured when saving. Code: CS001`)
         }
 
     }
