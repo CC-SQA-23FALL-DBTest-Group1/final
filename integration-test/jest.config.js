7b3718343e10081e1852a05cede4503a9f5ae4f1
badpolymer <roans_policy.07@icloud.com>
2023-12-13 15:31:53 -0500

    rename folder

--- integration-test/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testMatch: ["**/src/**/*.test.ts"]
-}
-
--- integration-test/scripts/wait-for-it.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-curl \
- -H 'content-type: application/json' \
- --retry 20 --retry-delay 5 \
- --retry-connrefused \
- "$1$2"
