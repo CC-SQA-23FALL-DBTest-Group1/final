f164eeb3de23e5060805e78e2e1f0dda6b8c3587
badpolymer <roans_policy.07@icloud.com>
2023-12-14 11:54:52 -0500

    Updat Customer Register

--- persistence-service/backend/src/strategy/postgresql/api/CustomerApi.ts
+++ persistence-service/backend/src/strategy/postgresql/api/CustomerApi.ts
@@ -34,26 +34,35 @@ export class CustomerApi {
     }
 
     async create(name: string, address: string, phoneNumber1: string,
-        phoneNumber2: string): Promise<Customer> {
+        phoneNumber2: string | null): Promise<Customer> {
 
         var customer = new Customer();
         customer.name = name.trim();
         customer.address = address.trim();
         customer.phoneNumber1 = phoneNumber1.trim();
-        customer.phoneNumber2 = phoneNumber2.trim();
 
-        if(customer.name.length == 0) {
+        const phoneNumRegex = /^(\+?(\d{1,3})?)?( ?\(\d{1,4}\))? ?\d{1,14}$/;
+        if (phoneNumber2) {
+
+            if (phoneNumRegex.test(phoneNumber2.trim())) {
+                customer.phoneNumber2 = phoneNumber2.trim();
+            }
+            else {
+                throw new Error(`Phone Number 2 is not Valid. Code: CU012`);
+            }
+        }
+
+
+        if (customer.name.length == 0) {
             throw new Error(`Name can not be empty. Code: CU005`);
         }
-        if(customer.address.length == 0) {
+        if (customer.address.length == 0) {
             throw new Error(`Address can not be empty. Code: CU006`);
         }
-        if(customer.phoneNumber1.length == 0) {
-            throw new Error(`Phone Number 1 can not be empty. Code: CU007`);
+        if (!phoneNumRegex.test(phoneNumber1.trim())) {
+            throw new Error(`Phone Number 1 is not Valid. Code: CU007`);
         }
-        if(customer.phoneNumber2.length == 0) {
-            throw new Error(`Phone Number 2 can not be empty. Code: CU008`);
-        }
+
 
         await this.#dataConnector.save(customer);
 
@@ -62,22 +71,32 @@ export class CustomerApi {
     }
 
     async updateByID(id: number, name: string, address: string,
-        phoneNumber1: string, phoneNumber2?: string): Promise<Customer> {
+        phoneNumber1: string, phoneNumber2?: string | null): Promise<Customer> {
 
         let customer = await this.getByID(id);
         customer.name = name.trim();
         customer.address = address.trim();
         customer.phoneNumber1 = phoneNumber1.trim();
-        customer.phoneNumber2 = phoneNumber2?.trim() ?? null;;
 
-        if(customer.name.length == 0) {
+        const phoneNumRegex = /^(\+?(\d{1,3})?)?( ?\(\d{1,4}\))? ?\d{1,14}$/;
+        if (phoneNumber2) {
+
+            if (phoneNumRegex.test(phoneNumber2.trim())) {
+                customer.phoneNumber2 = phoneNumber2.trim();
+            }
+            else {
+                throw new Error(`Phone Number 2 is not Valid. Code: CU013`);
+            }
+        }
+
+        if (customer.name.length == 0) {
             throw new Error(`Name can not be empty. Code: CU009`);
         }
-        if(customer.address.length == 0) {
+        if (customer.address.length == 0) {
             throw new Error(`Address can not be empty. Code: CU010`);
         }
-        if(customer.phoneNumber1.length == 0) {
-            throw new Error(`Phone Number 1 can not be empty. Code: CU011`);
+        if (!phoneNumRegex.test(phoneNumber1.trim())) {
+            throw new Error(`Phone Number 1 is not Valid. Code: CU011`);
         }
 
         await this.#dataConnector.save(customer);
@@ -86,8 +105,8 @@ export class CustomerApi {
 
     }
 
-    async deleteByID(id:number){
-        
+    async deleteByID(id: number) {
+
         let customer = await this.getByID(id);
 
         await this.#dataConnector.delete(customer);
