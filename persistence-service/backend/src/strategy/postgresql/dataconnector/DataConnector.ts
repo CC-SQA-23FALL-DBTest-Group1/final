58ef0f8d0e22762da1e69c87777518ad4a036bf5
badpolymer <roans_policy.07@icloud.com>
2023-12-13 22:20:11 -0500

    update DataConnector Interface

--- persistence-service/backend/src/strategy/postgresql/dataconnector/DataConnector.ts
+++ persistence-service/backend/src/strategy/postgresql/dataconnector/DataConnector.ts
@@ -2,7 +2,7 @@
 // Version 1
 // Last update: 2023-12-11
 export interface DataConnector<T> {
-  get: (...args: any) => Promise<T[]>;
+  get: (predicate:T) => Promise<T[]>;
   save: (entity: T) => Promise<void>;
   delete: (entity: T) => Promise<void>;
 }
