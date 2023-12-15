da5bcc21bcc74347eadb285f8027e6473c82e923
badpolymer <roans_policy.07@icloud.com>
2023-12-15 05:02:24 -0500

    add reset db

--- persistence-service/backend/src/server.ts
+++ persistence-service/backend/src/server.ts
@@ -13,14 +13,22 @@ import ApiRegister from "./strategy/postgresql/apiregister/ApiRegister";
   app.use(json());
 
   const datasource = await postgresDataSource.initialize();
-  datasource.undoLastMigration({transaction:`all`});
-  datasource.runMigrations();
+  // Temp Codes for reset database records
+  const entities = datasource.entityMetadatas;
 
-  new ApiRegister(datasource,app);
-  
+  for (const entity of entities) {
+    const repository = datasource.getRepository(entity.name);
+    let a = await repository.query(`TRUNCATE TABLE ${entity.tableName} RESTART IDENTITY CASCADE`);
+  }
+  // End of temp codes
+
+  await datasource.runMigrations();
+
+  new ApiRegister(datasource, app);
+
   app.get("/", (_, res) => {
-    return res.send("<h1>Final Assignment - Group 1 SEC 3</h1><br>"+
-    "<h2>By:<br>Yaonan Deng(Frederick)<br>Xingru Yao(Starley)<br>Keerthana Tikkisetty</h2>");
+    return res.send("<h1>Final Assignment - Group 1 SEC 3</h1><br>" +
+      "<h2>By:<br>Yaonan Deng(Frederick)<br>Xingru Yao(Starley)<br>Keerthana Tikkisetty</h2>");
   });
 
   app.listen(8000, () => {
