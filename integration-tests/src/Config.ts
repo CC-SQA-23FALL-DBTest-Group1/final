634300c2c524e3706ed7e41dde45d9bdce30e006
Xingru7 <starleyxry@gmail.com>
2023-12-15 22:56:11 -0500

    autotesst done

--- integration-tests/src/Config.ts
+++ integration-tests/src/Config.ts
@@ -1,5 +1,5 @@
 export function targetURL() {
-    //const targetUrl = `${process.env.TARGET_URL}`;
-    const targetUrl = `localhost`;
+    const targetUrl = `${process.env.TARGET_URL}`;
+    //const targetUrl = `localhost`;
     return targetUrl;
 }
